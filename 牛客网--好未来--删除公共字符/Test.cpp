0eacc6376cfc3fdfac3c4535144db586ab8d600d
SJRLL <1124335040@qq.com>
2020-04-16 22:25:29 +0800

    牛客网--好未来--删除公共字符

---
+++ 牛客网--好未来--删除公共字符/Test.cpp
@@ -0,0 +1,46 @@
+题目描述
+输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。例如，输入”They are students.”和”aeiou”，
+则删除之后的第一个字符串变成”Thy r stdnts.”
+输入描述 :
+每个测试输入包含2个字符串
+输出描述 :
+输出删除后的字符串
+示例1
+
+输入
+
+They are students.
+aeiou
+
+输出
+
+Thy r stdnts.
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string str1, str2;
+	getline(cin, str1);
+	getline(cin, str2);
+
+	string res = "";
+	for (int i = 0; i<str1.size(); ++i)
+	{
+		int flag = 1;
+		for (int j = 0; j<str2.size(); ++j)
+		{
+			if (str1[i] == str2[j])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			res += str1[i];
+		}
+	}
+	cout << res << endl;
+}
